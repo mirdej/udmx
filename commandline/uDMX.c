6dec15edaf0f65de0f317044c2bfedde3580e98d
Hugo Hackenstein <hns@zambur>
2017-02-02 02:34:24 +0100

    At least one argument should be given
    If uDMX is called without any argument you'll end up in an segfault
    because it still tries to access argv[1] which is poopoo.
    This untested patch should fix this behaviour.

--- commandline/uDMX.c
+++ commandline/uDMX.c
@@ -125,7 +125,7 @@ int                 nBytes;
         fprintf(stderr, "Could not find USB device \"uDMX\" with vid=0x%x pid=0x%x\n", USBDEV_SHARED_VENDOR, USBDEV_SHARED_PRODUCT);
         exit(1);
     }
-	if(argc < 3){
+	if(argc < 3 && argc > 1){
 		if (strcmp(argv[1], "-bootloader") == 0) {
 			nBytes = usb_control_msg(handle, USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_IN,
 									cmd_StartBootloader, 0, 0, buffer, sizeof(buffer), 5000);
